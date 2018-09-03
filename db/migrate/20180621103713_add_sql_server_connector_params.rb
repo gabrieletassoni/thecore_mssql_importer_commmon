c17a4b694efcb4bad7244e1d197cbfd148ca0dc5
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2018-09-04 00:22:30 +0200

    fix: removed gentili's configs

--- db/migrate/20180621103713_add_sql_server_connector_params.rb
+++ db/migrate/20180621103713_add_sql_server_connector_params.rb
@@ -1,12 +1,11 @@
 class AddSqlServerConnectorParams < ActiveRecord::Migration[5.2]
   def change
-    Settings.ns('mssql_connector').username = 'bancolini'
-    Settings.ns('mssql_connector').password = 'B@2(0l121'
-    Settings.ns('mssql_connector').host = '10.0.123.1'
-    Settings.ns('mssql_connector').port(kind: 'integer', default: 3499)
-    Settings.ns('mssql_connector').port = 3500
-    Settings.ns('mssql_connector').database = 'bancolini'
-    Settings.ns('mssql_connector').table_read = 'test'
+    Settings.ns('mssql_connector').username = 'sa'
+    Settings.ns('mssql_connector').password = 'your_password'
+    Settings.ns('mssql_connector').host = 'localhost'
+    Settings.ns('mssql_connector').port = 1433
+    Settings.ns('mssql_connector').database = 'test'
+    Settings.ns('mssql_connector').table_read = 'test_table'
     Settings.ns('mssql_connector').latest_migration_code = ''
   end
 end
